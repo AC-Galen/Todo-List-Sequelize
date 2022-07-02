cbcaa044a6b87db67826b71046b18b6d8f62fe90
AC-Galen <lottaie7359882@gmail.com>
2022-07-02 16:20:29 +0800

    feat: add todo and user model, add relations between user and todo models

--- models/user.js
+++ models/user.js
@@ -2,11 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
-    eamil: DataTypes.STRING,
+    email: DataTypes.STRING,
     password: DataTypes.STRING
   }, {});
   User.associate = function(models) {
     // associations can be defined here
+    User.hasMany(models.Todo)
   };
   return User;
 };
